862688d5f09669e77e0772c879e988b88a33a68f
zhangshaolong.1024 <zhangshaolong.1024@bytedance.com>
2021-01-24 14:04:17 +0800

    拆分mock功能，对外提供根据path生成mock数据的工具

--- src/mock.js
+++ src/mock.js
@@ -16,6 +16,96 @@ const metaReg = /^\s*\/\*([\s\S]*?)\*\//m
 
 const isMockDataReg = /^\s*(?:function|\{)/
 
+const getMockDataFromFilePath = (pathName, params, request, response) => {
+  const exist = fs.existsSync(pathName)
+  if (exist) {
+    let mtime = fs.statSync(pathName).mtime.getTime()
+    let cachedApi = cachedApis[pathName]
+    if (!cachedApi || cachedApi.mtime !== mtime) {
+      try {
+        let content = new String(fs.readFileSync(pathName, encoding), encoding).trim()
+        // 支持在mock配置一些描述信息，实现对API生成接口文档
+        let matched = true
+        while (matched) {
+          matched = false
+          content = content.replace(metaReg, (all, contents) => {
+            matched = true
+            return ''
+          })
+        }
+        content = content.replace(semReg, '') // 有的编辑器会自动在最后加上分号
+        if (isMockDataReg.test(content)) {
+          content = 'return (' + content + ')'
+        }
+        let result = Function(content)()
+        cachedApis[pathName] = cachedApi = {
+          result,
+          mtime
+        }
+      } catch (e) {
+        try {
+          const content = fs.readFileSync(pathName, 'binary')
+          return {
+            writeHead: [200],
+            output: [content, 'binary']
+          }
+        } catch (e) {
+          return {
+            writeHead: [500],
+            output: [e.message, encoding],
+            parser: JSON.stringify.bind(JSON)
+          }
+        }
+      }
+    }
+    let result = cachedApi.result
+    if (typeof result === 'function') {
+      result = result(params, {
+        require,
+        request,
+        response,
+        __dirname: path.resolve(pathName, '..'),
+        tools: {} // 后续可以进行mock的功能扩展，比如提供生成range数据等等
+      })
+    }
+    const sleep = result.sleep
+    if (!isNaN(sleep)) {
+      try {
+        let copy = JSON.parse(JSON.stringify(result))
+        delete copy.sleep
+        return {
+          writeHead: [200, { 'Content-Type': 'text/plain;charset=' + encoding }],
+          output: [copy, encoding],
+          parser: JSON.stringify.bind(JSON),
+          sleep: sleep
+        }
+      } catch (e) {
+        return {
+          writeHead: [200, { 'Content-Type': 'text/plain;charset=' + encoding }],
+          output: [{
+            code: 500,
+            url: reqUrl,
+            e: e
+          },
+          encoding],
+          parser: JSON.stringify.bind(JSON)
+        }
+      }
+    } else {
+      return {
+        writeHead: [200, { 'Content-Type': 'text/plain;charset=' + encoding }],
+        output: [result, encoding],
+        parser: JSON.stringify.bind(JSON)
+      }
+    }
+  } else {
+    return {
+      writeHead: [500],
+      output: [pathName + ' file is not existed~', encoding]
+    }
+  }
+}
+
 const doMock = (pathName, request, response, params, options) => {
   let mockPath = (options.mockConfig && options.mockConfig.path) || 'mock'
   try {
@@ -39,83 +129,30 @@ const doMock = (pathName, request, response, params, options) => {
       }
     }
     pathName += (options.mockConfig && options.mockConfig.ext) || '.js'
-    fs.exists(pathName, (exist) => {
-      if (exist) {
-        let mtime = fs.statSync(pathName).mtime.getTime()
-        let cachedApi = cachedApis[pathName]
-        if (!cachedApi || cachedApi.mtime !== mtime) {
-          try {
-            let content = new String(fs.readFileSync(pathName, encoding), encoding).trim()
-            // 支持在mock配置一些描述信息，实现对API生成接口文档            
-            let matched = true
-            while (matched) {
-              matched = false
-              content = content.replace(metaReg, (all, contents) => {
-                matched = true
-                return ''
-              })
+    const result = getMockDataFromFilePath(pathName, params, request, response)
+    if (!isNaN(result.sleep)) {
+      setTimeout(() => {
+        response.writeHead.apply(response, result.writeHead)
+        response.end.apply(response, result.output.map((item, idx) => {
+          if (idx === 0) {
+            if (result.parser) {
+              return result.parser(item)
             }
-            content = content.replace(semReg, '') // 有的编辑器会自动在最后加上分号
-            if (isMockDataReg.test(content)) {
-              content = 'return (' + content + ')'
-            }
-            let result = Function(content)()
-            cachedApis[pathName] = cachedApi = {
-              result,
-              mtime
-            }
-          } catch (e) {
-            try {
-              const content = fs.readFileSync(pathName, 'binary')
-              response.writeHead(200)
-              response.write(content, 'binary')
-              response.end()
-            } catch (e) {
-              response.writeHead(500)
-              response.end(JSON.stringify(e.message))
-            }
-            return
+          }
+          return item
+        }))
+      }, result.sleep)
+    } else {
+      response.writeHead.apply(response, result.writeHead)
+      response.end.apply(response, result.output.map((item, idx) => {
+        if (idx === 0) {
+          if (result.parser) {
+            return result.parser(item)
           }
         }
-        let result = cachedApi.result
-        if (typeof result === 'function') {
-          result = result(params, {
-            require,
-            request,
-            response,
-            __dirname: path.resolve(pathName, '..'),
-            tools: {} // 后续可以进行mock的功能扩展，比如提供生成range数据等等
-          })
-        }
-        response.writeHead(200, { 'Content-Type': 'text/plain;charset=' + encoding })
-        if (!isNaN(result.sleep)) {
-          setTimeout(() => {
-            try {
-              let copy = JSON.parse(JSON.stringify(result))
-              delete copy.sleep
-              response.end(JSON.stringify(copy), encoding)
-            } catch (e) {
-              response.end(
-                JSON.stringify({
-                  code: 500,
-                  url: reqUrl,
-                  e: e
-                }),
-                encoding
-              )
-            }
-          }, result.sleep)
-        } else {
-          if (typeof result !== 'string') {
-            result = JSON.stringify(result)
-          }
-          response.end(result, encoding)
-        }
-      } else {
-        response.writeHead(500)
-        response.end(pathName + ' file is not existed~')
-      }
-    })
+        return item
+      }))
+    }
   } catch (e) {
     response.writeHead(500)
     response.end(JSON.stringify(e.message))
@@ -123,5 +160,6 @@ const doMock = (pathName, request, response, params, options) => {
 }
 
 module.exports = {
-  doMock
+  doMock,
+  getMockDataFromFilePath
 }
